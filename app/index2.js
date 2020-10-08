470725bbace133c14fdd526a71449459bc4c53bf
Matubo <Matbei14@yandex.ru>
2020-10-08 20:12:52 +0300

    add react-router-dom+build command

--- app/index2.js
+++ app/index2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useReducer } from "react";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -14,7 +15,13 @@ function App() {
   const [state, dispatch] = useReducer(reducer, 0);
   return (
     <div className="test">
-      <div>test {state}</div>
+      <div>test sum={state}</div>
+      <Router prop={dispatch}>
+        <Link to="/">*</Link>
+        <Link to="/decrement">#</Link>
+        <Route exact path="/" component={Increment} prop={1}></Route>
+        <Route exact path="/decrement" component={Decrement} prop={2}></Route>
+      </Router>
       <button
         onClick={() => {
           dispatch({ type: "decrement" });
@@ -32,4 +39,13 @@ function App() {
     </div>
   );
 }
+
+function Increment(a) {
+  return <button onClick={() => {}}>*</button>;
+}
+
+function Decrement(a) {
+  return <button onClick={() => {}}>#</button>;
+}
+
 export default App;
